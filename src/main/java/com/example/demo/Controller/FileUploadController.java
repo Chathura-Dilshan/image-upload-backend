2b7269a59d9ede20e8ef68e19c8d1462889cefbc
chathurad <chathurad@itx360.com>
2020-04-12 15:25:00 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Controller/FileUploadController.java
@@ -0,0 +1,58 @@
+package com.example.demo.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+@RestController
+@CrossOrigin(value = "http://localhost:3000")
+public class FileUploadController {
+    private static final Logger logger = Logger.getLogger(FileUploadController.class.getName());
+    private static String UPLOADED_FOLDER = "E://temp//";
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadData(@RequestParam("file") MultipartFile file ) throws Exception {
+        if (file == null) {
+            throw new RuntimeException("You must select the a file for uploading");
+        }
+
+        InputStream inputStream = file.getInputStream();
+        String originalName = file.getOriginalFilename();
+        String name = file.getName();
+        String contentType = file.getContentType();
+        long size = file.getSize();
+        logger.info("inputStream: " + inputStream);
+        logger.info("originalName: " + originalName);
+        logger.info("name: " + name);
+        logger.info("contentType: " + contentType);
+        logger.info("size: " + size);
+        // Do processing with uploaded file data in Service layer
+
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            logger.info("successful: " + "successful");
+
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+        return new ResponseEntity<String>(originalName, HttpStatus.OK);
+    }
+}
